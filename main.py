466f720056f4bcd011863edfc42dfd6e18031fe8
Mehul Kumar <84561537+Mehulkumar123@users.noreply.github.com>
2023-02-26 18:23:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,116 +1,93 @@
 import tkinter as tk
-import random
-
-# Define constants
 CELL_SIZE = 20
-ROWS = 25
-COLS = 25
-
-# Create the Cell class
+GRID_WIDTH = 30
+GRID_HEIGHT = 20
+BG_COLOR = 'white'
+CELL_COLOR = 'black'
 class Cell:
-    def __init__(self, row, col, state=0):
-        self.row = row
-        self.col = col
-        self.state = state
-        self.rect_id = None
-
-    def draw(self, canvas):
-        x0 = self.col * CELL_SIZE
-        y0 = self.row * CELL_SIZE
-        x1 = (self.col + 1) * CELL_SIZE
-        y1 = (self.row + 1) * CELL_SIZE
-        if self.state == 1:
-            fill = "black"
-        else:
-            fill = "white"
-        self.rect_id = canvas.create_rectangle(x0, y0, x1, y1, fill=fill, outline="gray")
-
-    def toggle(self, canvas):
-        self.state = 1 - self.state
-        canvas.itemconfigure(self.rect_id, fill="black" if self.state == 1 else "white")
-
-# Initialize the grid of cells
-grid = [[Cell(i, j) for j in range(COLS)] for i in range(ROWS)]
-
-# Create the GUI
-root = tk.Tk()
-root.title("Game of Life")
-
-canvas = tk.Canvas(root, width=CELL_SIZE*COLS, height=CELL_SIZE*ROWS)
-canvas.pack()
-
-def create_grid():
-    for row in grid:
-        for cell in row:
-            cell.draw(canvas)
-
-def change_colour(event):
-    x = event.x // CELL_SIZE
-    y = event.y // CELL_SIZE
-    cell = grid[y][x]
-    cell.toggle(canvas)
-
-canvas.bind("<Button-1>", change_colour)
-
-def get_neighbours(row, col):
-    neighbours = []
-    for i in range(max(row-1, 0), min(row+2, ROWS)):
-        for j in range(max(col-1, 0), min(col+2, COLS)):
-            if (i, j) != (row, col):
-                neighbours.append(grid[i][j])
-    return neighbours
-
-def change_cell_state(cell):
-    neighbours = get_neighbours(cell.row, cell.col)
-    live_count = sum(neighbour.state for neighbour in neighbours)
-    if cell.state == 1:
-        if live_count < 2 or live_count > 3:
-            return 0
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.state = 0
+        self.rect = None
+def createGrid(canvas):
+    global cells
+    cells = [[Cell(x, y) for y in range(GRID_HEIGHT)] for x in range(GRID_WIDTH)]
+    for x in range(GRID_WIDTH):
+        for y in range(GRID_HEIGHT):
+            rect = canvas.create_rectangle(
+                x*CELL_SIZE, y*CELL_SIZE, (x+1)*CELL_SIZE, (y+1)*CELL_SIZE,
+                fill=BG_COLOR, outline='gray'
+            )
+            cells[x][y].rect = rect
+def changeColor(event):
+    global cells
+    x, y = event.x // CELL_SIZE, event.y // CELL_SIZE
+    cell = cells[x][y]
+    if cell.state == 0:
+        canvas.itemconfig(cell.rect, fill=CELL_COLOR)
+        cell.state = 1
     else:
-        if live_count == 3:
-            return 1
-    return cell.state
-
-def start_game():
+        canvas.itemconfig(cell.rect, fill=BG_COLOR)
+        cell.state = 0
+def changeCellState(cell):
+    global cells
+    x, y = cell.x, cell.y
+    neighbors = []
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if i == 0 and j == 0:
+                continue
+            if x+i < 0 or x+i >= GRID_WIDTH or y+j < 0 or y+j >= GRID_HEIGHT:
+                continue
+            neighbors.append(cells[x+i][y+j])
+    live_neighbors = sum(n.state for n in neighbors)
+    if cell.state == 1 and (live_neighbors < 2 or live_neighbors > 3):
+        cell.next_state = 0
+    elif cell.state == 0 and live_neighbors == 3:
+        cell.next_state = 1
+    else:
+        cell.next_state = cell.state
+def startGame():
+    global cells, begin_id
+    for x in range(GRID_WIDTH):
+        for y in range(GRID_HEIGHT):
+            changeCellState(cells[x][y])
+    for x in range(GRID_WIDTH):
+        for y in range(GRID_HEIGHT):
+            cell = cells[x][y]
+            cell.state = cell.next_state
+            if cell.state == 0:
+                canvas.itemconfig(cell.rect, fill=BG_COLOR)
+            else:
+                canvas.itemconfig(cell.rect, fill=CELL_COLOR)
+    begin_id = canvas.after(100, startGame)
+def pauseGame():
     global begin_id
-    new_grid = [[Cell(i, j, change_cell_state(cell)) for j, cell in enumerate(row)] for i, row in enumerate(grid)]
-    grid[:] = new_grid
-    for row in grid:
-        for cell in row:
-            cell.draw(canvas)
-    begin_id = root.after(500, start_game)
-
-def pause_game():
-    root.after_cancel(begin_id)
-
-def exit_game():
+    canvas.after_cancel(begin_id)
+def exitGame():
     root.destroy()
-
 def reset():
-    global begin_id
-    if begin_id:
-        root.after_cancel(begin_id)
-    grid[:] = [[Cell(i, j, random.randint(0, 1)) for j in range(COLS)] for i in range(ROWS)]
-    for row in grid:
-        for cell in row:
-            cell.draw(canvas)
-
-
+    global cells, begin_id
+    canvas.after_cancel(begin_id)
+    cells = [[Cell(x, y) for y in range(GRID_HEIGHT)] for x in range(GRID_WIDTH)]
+    for x in range(GRID_WIDTH):
+        for y in range(GRID_HEIGHT):
+            canvas.itemconfig(cells[x][y].rect, fill=BG_COLOR)
+            cells[x][y].state = 0
+root = tk.Tk()
+root.title('Conway\'s Game of Life')
+canvas = tk.Canvas(root, width=GRID_WIDTH*CELL_SIZE, height=GRID_HEIGHT*CELL_SIZE)
+canvas.pack()
+createGrid(canvas)
+canvas.bind('<Button-1>', changeColor)
+start_button = tk.Button(root, text='Start', command=startGame)
+start_button.pack(side='left')
+pause_button = tk.Button(root, text='Pause', command=pauseGame)
+pause_button.pack(side='left')
+reset_button = tk.Button(root, text='Reset', command=reset)
+reset_button.pack(side='left')
+exit_button = tk.Button(root, text='Exit', command=exitGame)
+exit_button.pack(side='left')
 begin_id = None
-
-reset_button = tk.Button(root, text="Reset", command=reset)
-reset_button.pack(side=tk.LEFT)
-
-start_button = tk.Button(root, text="Start", command=start_game)
-start_button.pack(side=tk.LEFT)
-
-pause_button = tk.Button(root, text="Pause", command=pause_game)
-pause_button.pack(side=tk.LEFT)
-
-exit_button = tk.Button(root, text="Exit", command=exit_game)
-exit_button.pack(side=tk.LEFT)
-
-create_grid()
-reset()
-
 root.mainloop()
